126b67fbe21cdd3a5db075744aef402b3acac974
sergiysova <serj.sova@gmail.com>
2018-01-19 20:37:13 +0200

    Changed addres, and added new page
    Page "Creatives" Added

---
+++ creatives/js/script.js
@@ -0,0 +1,41 @@
+ $(document).ready(
+    function() {
+        $("html").niceScroll({
+        	cursorcolor:"#000",
+        	cursorborder: "none",
+        	cursorborderradius:"8px"	
+    });
+
+    chooseHeader();
+
+
+});
+
+$(document).scroll(function () {
+
+	chooseHeader();
+});
+
+
+
+$("#scroll_down").click(function(){
+	$("html,body").animate({scrollTop:1300},"slow");
+});
+
+
+function chooseHeader(){
+
+	if(	($(window).width() <= 768)){
+		$('header').addClass('collapsed');
+	}
+	else {
+		var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	if(scrollTop>300){
+		$('header').addClass('collapsed');
+	}
+	else {
+		$('header').removeClass('collapsed');
+	}
+	}
+	
+}
